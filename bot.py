74bc9dadc3ca30229641660aeeaf1e1e6b9a82e0
ali <ali@alineer.com>
2019-03-19 21:05:18 -0400

    set up for master and heroku

--- bot.py
+++ bot.py
@@ -9,22 +9,20 @@ import random
 log = logging.getLogger("bot")
 log.setLevel(logging.DEBUG)
 
-# CONSUMER_API = os.environ["CONSUMER_API_KEY"]
-# CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-# ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-# ACCESS_SECRET = os.environ["ACCESS_SECRET"]
+CONSUMER_API = os.environ["CONSUMER_API_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 
-# STEM = "Oh, you think you are kvlt, I only listen to "
-STEMS = ["Oh, you think you are kvlt, I only listen to ", "if you were really into metal you would know ", "Hey, poser, I'm the founder of ", "These fuckin' kids never heard of ", "I remember the days before hipsters and posers infecting "]
-# MID = [f'I was listening to {genre} before you were born.', f"you like {genre}? ugh. that's for posers."]
-# BASE = [" IS FOR REAL METALHEADS ONLY!!!!"]
-# auth = tweepy.OAuthHandler(CONSUMER_API, CONSUMER_SECRET)
-# auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-# api = tweepy.API(auth)
+auth = tweepy.OAuthHandler(CONSUMER_API, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+genre = generate_genre(140) # half the tweet
+STEMS = [f"Oh, you think you are kvlt, I only listen to {genre}.", f"if you were really into metal you would know {genre}.", f"Hey, poser, I'm the founder of {genre}.", f"These fuckin' kids never heard of {genre}.", f"I remember the days before hipsters and posers infecting {genre}.", f"{genre} IS FOR REAL METALHEADS ONLY!!!!", f'I was listening to {genre} before you were born.', f"you like {genre}? ugh. that's for posers."]
 STEM = random.choice(STEMS)
-print(f"bot - stem: {STEM}")
-print("bot - generating sentence")
-total = 280 - len(STEM)
-sentence = f'{STEM}{generate_genre(total)}'
+if random.randint(0, 10) == 1:
+    sentence = f'{STEM}'.upper()
+else:
+    sentence = f'{STEM}'
 print(f'bot - finished sentence: {sentence}')
-# api.update_status(sentence)
+api.update_status(sentence)
