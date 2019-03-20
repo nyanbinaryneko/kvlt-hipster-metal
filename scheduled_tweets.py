1b7bdd61edc6826df9c7c421b8539f58900bbdb4
ali <ali@alineer.com>
2019-03-19 21:33:26 -0400

    set up tweepy for scheduled tweets

---
+++ scheduled_tweets.py
@@ -0,0 +1,8 @@
+import tweepy
+import os
+
+CONSUMER_API = os.environ["CONSUMER_API_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_SECRET = os.environ["ACCESS_SECRET"]
+
