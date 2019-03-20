240038f3c6a66320b664d612b306f317f3697e17
ali <ali@alineer.com>
2019-03-19 20:24:45 -0400

    set up for local dev

--- bot.py
+++ bot.py
@@ -8,21 +8,21 @@ import logging
 log = logging.getLogger("bot")
 log.setLevel(logging.DEBUG)
 
-CONSUMER_API = os.environ["CONSUMER_API_KEY"]
-CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-ACCESS_SECRET = os.environ["ACCESS_SECRET"]
+# CONSUMER_API = os.environ["CONSUMER_API_KEY"]
+# CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+# ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+# ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 
-STEM = "Oh, you think you are kvlt, I only listen to "
-# STEMS = ["Oh, you think you are kvlt, I only listen to ", "if you were really into metal you would know ", "Hey, poser, I'm the founder of ", "These fuckin' kids never heard of ", "I remember the days before hipsters and posers infecting "], 
+# STEM = "Oh, you think you are kvlt, I only listen to "
+STEMS = ["Oh, you think you are kvlt, I only listen to ", "if you were really into metal you would know ", "Hey, poser, I'm the founder of ", "These fuckin' kids never heard of ", "I remember the days before hipsters and posers infecting "], 
 # MID = [f'I was listening to {genre} before you were born.', f"you like {genre}? ugh. that's for posers."]
 # BASE = [" IS FOR REAL METALHEADS ONLY!!!!"]
-auth = tweepy.OAuthHandler(CONSUMER_API, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-api = tweepy.API(auth)
+# auth = tweepy.OAuthHandler(CONSUMER_API, CONSUMER_SECRET)
+# auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+# api = tweepy.API(auth)
 
 print("bot - generating sentence")
 total = 280 - len(STEM)
 sentence = f'{STEM}{generate_genre(total)}'
 print(f'bot - generating sentence: {sentence}')
-api.update_status(sentence)
+# api.update_status(sentence)
