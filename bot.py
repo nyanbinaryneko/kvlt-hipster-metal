b3c85ea287b4ea06b94fb4a11a34f5eb14c1c2ed
ali <ali@alineer.com>
2019-03-19 20:45:07 -0400

    added more stems

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import tweepy
 import os
 import markovify
 import logging
+import random
 
 log = logging.getLogger("bot")
 log.setLevel(logging.DEBUG)
@@ -14,15 +15,16 @@ log.setLevel(logging.DEBUG)
 # ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 
 # STEM = "Oh, you think you are kvlt, I only listen to "
-STEMS = ["Oh, you think you are kvlt, I only listen to ", "if you were really into metal you would know ", "Hey, poser, I'm the founder of ", "These fuckin' kids never heard of ", "I remember the days before hipsters and posers infecting "], 
+STEMS = ["Oh, you think you are kvlt, I only listen to ", "if you were really into metal you would know ", "Hey, poser, I'm the founder of ", "These fuckin' kids never heard of ", "I remember the days before hipsters and posers infecting "]
 # MID = [f'I was listening to {genre} before you were born.', f"you like {genre}? ugh. that's for posers."]
 # BASE = [" IS FOR REAL METALHEADS ONLY!!!!"]
 # auth = tweepy.OAuthHandler(CONSUMER_API, CONSUMER_SECRET)
 # auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 # api = tweepy.API(auth)
-
+STEM = random.choice(STEMS)
+print(f"bot - stem: {STEM}")
 print("bot - generating sentence")
 total = 280 - len(STEM)
 sentence = f'{STEM}{generate_genre(total)}'
-print(f'bot - generating sentence: {sentence}')
+print(f'bot - finished sentence: {sentence}')
 # api.update_status(sentence)
