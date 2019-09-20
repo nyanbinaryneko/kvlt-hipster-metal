4c65b97248d7aee15e9d49e7e736975f55d504bd
Ali <3050609+alikoneko@users.noreply.github.com>
2019-09-20 10:02:24 -0400

    graphic design is my passion

--- metal_albumify.py
+++ metal_albumify.py
@@ -1,3 +1,8 @@
 from PIL import Image
-img = Image.open('./corpus/img/test.jpg').convert('L')
-img.save('./corpus/img/output/out1.jpg')
+forground_img = Image.open('./corpus/img/testlogo.png') # open test logo
+background_img = Image.open('./corpus/img/test.jpg') # open background
+
+background_img.paste(forground_img, (0,0), forground_img) # paste her on?
+img = background_img.convert("L") #transformation
+img.save('./corpus/img/output/out2.jpg')
+
