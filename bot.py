481b5f2240f071d6c3a6ef655a1f56e217560df0
Ali <3050609+alikoneko@users.noreply.github.com>
2019-12-07 11:37:19 -0500

    update!

--- bot.py
+++ bot.py
@@ -1,30 +1,18 @@
-from markov import generate_genre
-import time
 import tweepy
 import os
-import markovify
-import logging
-import random
+from poser_takes import generate_poser_take
 
-log = logging.getLogger("bot")
-log.setLevel(logging.DEBUG)
-
-CONSUMER_API = os.environ["CONSUMER_API_KEY"]
-CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-ACCESS_SECRET = os.environ["ACCESS_SECRET"]
-
-auth = tweepy.OAuthHandler(CONSUMER_API, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-api = tweepy.API(auth)
-genre = generate_genre(140) # half the tweet
-STEMS = [f"Oh, you think you are kvlt, I only listen to {genre}.", f"if you were really into metal you would know {genre}.", f"Hey, poser, I'm the founder of {genre}.", f"These fuckin' kids never heard of {genre}.", f"I remember the days before hipsters and posers infecting {genre}.", f"{genre} IS FOR REAL METALHEADS ONLY!!!!", f'I was listening to {genre} before you were born.', f"you like {genre}? ugh. that's for posers."]
-STEM = random.choice(STEMS)
-if random.randint(0, 10) == 1:
-    sentence = f'{STEM}'.upper()
-else:
-    sentence = f'{STEM}'
+DEBUG = os.environ["DEBUG"].lower() == "true"
+sentence = generate_poser_take()
 print(f'bot - finished sentence: {sentence}')
-if "rac" in sentence.lower() or "oi!" in sentence.lower():
-    sentence = f'{genre.upper()} IS FOR FUCKING POSERS. LISTEN TO REAL METAL YOU PANDA COSPLAYERS!'
-api.update_status(sentence)
+if not DEBUG:
+    CONSUMER_API = os.environ["CONSUMER_API_KEY"]
+    CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+    ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+    ACCESS_SECRET = os.environ["ACCESS_SECRET"]
+
+    auth = tweepy.OAuthHandler(CONSUMER_API, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    api.update_status(sentence)
+    print(f'bot - status updated')
