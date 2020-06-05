00f3efea58643767ab6c7cb1021ddf83149378f7
ali neer <ali@alineer.com>
2020-06-05 13:13:00 -0400

    request 26 done

--- poser_takes.py
+++ poser_takes.py
@@ -2,53 +2,57 @@ from markov import generate_genre
 import random
 
 def generate_poser_take():
-    genre_long = [ generate_genre(140) ]
-    genre_short = [ generate_genre(100) ]
+    genre = generate_genre(140)
+
     STEMS = [
             { 
-                "sentence": f"Oh, you think you are kvlt, I only listen to {genre_long[0]}.",
-                "genres": genre_long
+                "sentence": f"Oh, you think you are kvlt, I only listen to {genre}.",
+                "genres": genre
             }, 
             {
-                "sentence": f"if you were really into metal you would know {genre_long[0]}.",
-                "genres": genre_long
+                "sentence": f"if you were really into metal you would know {genre}.",
+                "genres": genre
             }, 
             {
-                "sentence": f"Hey, poser, I'm the founder of {genre_long[0]}.",
-                "genres": genre_long
+                "sentence": f"Hey, poser, I'm the founder of {genre}.",
+                "genres": genre
             }, 
             {
-                "sentence": f"These fuckin' kids never heard of {genre_long[0]}.",
-                "genres": genre_long
+                "sentence": f"These fuckin' kids never heard of {genre}.",
+                "genres": genre
             }, 
             {
-                "sentence": f"I remember the days before hipsters and posers infecting {genre_long[0]}.",
-                "genres": genre_long
+                "sentence": f"I remember the days before hipsters and posers infecting {genre}.",
+                "genres": genre
             }, 
             {
-                "sentence": f"{genre_long[0]} IS FOR REAL METALHEADS ONLY!!!!",
-                "genres": genre_long
+                "sentence": f"{genre} IS FOR REAL METALHEADS ONLY!!!!",
+                "genres": genre
             }, 
             {
-                "sentence":f'I was listening to {genre_long[0]} before you were born.', 
-                "genres": genre_long
+                "sentence":f'I was listening to {genre} before you were born.', 
+                "genres": genre
             }, 
             {
-                "sentence": f"you like {genre_long[0]}? ugh. that's for posers.",
-                "genres": genre_long
+                "sentence": f"you like {genre}? ugh. that's for posers.",
+                "genres": genre
             },
             {
                 "sentence": f'Tool is just Radiohead for rape apologists.',
                 "genres": []
             },
             {
-                "sentence": f'Geez, who the fuck invented post-{genre_long[0]}core? It fuckin\' sucks.',
-                "genres": genre_long
+                "sentence": f'Geez, who the fuck invented post-{genre}core? It fuckin\' sucks.',
+                "genres": genre
             },
             {
-                "sentence": f'Can anyone actually name a band that is {genre_short[0]}? Spotify says they are a thing...but I don\'t believe them..',
-                "genres": genre_short
+                "sentence": f'Can anyone actually name a band that is {genre}? Spotify says they are a thing...but I don\'t believe them..',
+                "genres": genre
             },
+            {
+                "sentence": f'{genre} was better in the 80s.',
+                "genres": genre
+            }
             ]
     tweet = random.choice(STEMS)
     if random.randint(0, 10) == 1:
@@ -59,6 +63,6 @@ def generate_poser_take():
         if(len(genres) > 1):
             genre = " and ".join(genres)
         else:
-            genre = genres[0]
+            genre = genres
         tweet["sentence"] = f'{genre}. IS FOR FUCKING POSERS! LISTEN TO REAL METAL YOU PANDA COSPLAYING COWARDS!'.upper()
     return  tweet["sentence"]
